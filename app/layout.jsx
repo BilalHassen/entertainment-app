a5fefb9ed75173d60d9a37e346442f4caa213ff6
BilalHassen <bilalhassen097@gmail.com>
2024-10-15 14:37:02 -0400

    add Outfit font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,31 @@
 // Importing the global CSS file to apply styles throughout the entire application.
 import "../styles/global.css";
-
+// Importing the Outfit font from Google Fonts with Next.js
+import { Outfit } from "@next/font/google";
 // Importing React to create functional components.
 import React from "react";
 
 // This object defines metadata for the application, such as the title and description.
-// It could be used in Next.js to set HTML head elements dynamically.
 export const metadata = {
   title: "Movies App", // The title of the app, displayed in the browser tab or window title.
   description: "Discover & organize your favourite movies and videos", // A brief description of the app.
 };
 
+// Corrected font import with the Outfit font
+const outfitFont = Outfit({
+  weight: ["200", "300", "400"],
+  subsets: ["latin"], // Make sure to specify the subset
+});
+
 // The main layout component for the entire application.
-// This component wraps all pages in a consistent layout.
 const RootLayout = ({ children }) => {
-  // The RootLayout component is using the 'children' prop. This prop will contain the content (components)
-  // that are passed to RootLayout when it is used in other parts of the app, effectively rendering the page contents within this layout.
+  // The RootLayout component is using the 'children' prop.
+  // This prop will contain the content (components) passed to RootLayout when used elsewhere in the app.
 
   return (
     <html lang="en">
-      {" "}
       {/* Declaring the language for the document to be English */}
-      <body>
+      <body className={outfitFont.className}>
         {/* This is a container for the main layout styles */}
         <div className="main">
           {/* This div is styled with a class 'gradient', which likely adds a gradient background */}
