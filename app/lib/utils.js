308c18257448e8962fce68b562c90abd669a7b3e
BilalHassen <bilalhassen097@gmail.com>
2024-10-24 22:06:15 -0400

    fix url

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const getTrendingVideos = async () => {
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const url = `${baseURL}/videos/trending`;
+  const url = `${baseURL}/api/videos/trending`;
+  console.log(url);
   try {
     const response = await axios.get(url);
     return response.data;
