e317a16b9ee5f5703d5438fa263791388b57b627
BilalHassen <bilalhassen097@gmail.com>
2025-01-14 13:40:45 -0500

    create component and folder for RecommendedVideos

---
+++ app/src/components/RecommendedVideos/RecommendedVideos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RecommendedVideos({ recommendedVideos }) {
+  return (
+    <>
+      <h1>Recommended for you</h1>
+      <div></div>
+    </>
+  );
+}
+
+export default RecommendedVideos;
