1651bfc9a96001663252f5a3193e4f566e26b014
BilalHassen <bilalhassen097@gmail.com>
2025-01-17 20:05:56 -0500

    change name of controller variable in videos.js

---
+++ api/routes/movies.js
@@ -0,0 +1,4 @@
+// create an isolated instance of a router
+// allows to create routes that are mountable to a specific url path
+const router = require("express").Router();
+const userController = require("../controllers/moviesController");
--- api/routes/videos.js
+++ api/routes/videos.js
@@ -1,7 +1,7 @@
 // create an isolated instance of a router
 // allows to create routes that are mountable to a specific url path
 const router = require("express").Router();
-const userController = require("../controllers/videosController");
+const videosController = require("../controllers/videosController");
 
 // middleware that is specific to this router
 const timeLog = (req, res, next) => {
@@ -12,7 +12,7 @@ router.use(timeLog);
 
 // home route will call the getTrendingVideos function
 // when the route is hit
-router.route("/trending-videos").get(userController.getTrendingVideos);
-router.route("/recommended-videos").get(userController.getRecommendedVideos);
+router.route("/trending-videos").get(videosController.getTrendingVideos);
+router.route("/recommended-videos").get(videosController.getRecommendedVideos);
 // use node module to export the route
 module.exports = router;
