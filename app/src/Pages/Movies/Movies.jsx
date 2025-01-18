53c2a51171c973e2b3ce9e868eca49a4d3bc21d6
BilalHassen <bilalhassen097@gmail.com>
2025-01-17 20:54:17 -0500

    fix movies query to remove duplicates

--- app/src/Pages/Movies/Movies.jsx
+++ app/src/Pages/Movies/Movies.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { useVideosContext } from "../../context/videoContext";
 function Movies() {
+  const { movies } = useVideosContext(); // Access the context
+  console.log(movies);
+
   return (
     <div>
       <h1>Movies Page</h1>
