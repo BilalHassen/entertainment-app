cf8acdb9ab15dedf3572cfa4ba63d98af3d15805
BilalHassen <bilalhassen097@gmail.com>
2025-01-17 19:30:15 -0500

    add Movie page with stylesheet

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from "react";
 import "./App.scss";
 import { VideoProvider } from "./context/videoContext";
 import Home from "./Pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
       <VideoProvider>
-        <Home />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />{" "}
+            <Route path="/tv" element={<Home />} />
+            <Route path="/movies" element={<Home />} />
+            <Route path="/bookmarked" element={<Home />} />
+          </Routes>
+        </Router>
       </VideoProvider>
     </>
   );
