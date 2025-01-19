1525f53474424112541eec67ba70c2801f69e9f1
BilalHassen <bilalhassen097@gmail.com>
2025-01-19 15:21:09 -0500

    add routing for tvShows

---
+++ api/controllers/tvController.js
@@ -0,0 +1,32 @@
+const knex = require("knex")(require("../knexfile"));
+const {
+  collectDuplicateThumbnails,
+} = require("../helper/collectDuplicateThumbnails");
+
+async function getTvShows(req, res) {
+  try {
+    const tvResponse = await knex("movies")
+      .join("thumbnails", "movies.id", "=", "thumbnails.movie_id")
+      .where("movies.category", "=", "TV Series")
+      .select(
+        "movies.id",
+        "movies.title",
+        "movies.year",
+        "movies.category",
+        "movies.rating",
+        "movies.is_bookmarked",
+        "movies.is_trending",
+        "movies.is_recommended",
+        "thumbnails.url"
+      );
+
+    const formattedTvShows = collectDuplicateThumbnails(tvResponse);
+    res.status(200).json(formattedTvShows);
+  } catch (err) {
+    res.status(500).json(`LoL an error occured ${err}`);
+  }
+}
+
+module.exports = {
+  getTvShows,
+};
--- api/server.js
+++ api/server.js
@@ -7,12 +7,16 @@ app.use(cors());
 // get the route handlers
 const videos = require("./routes/videos");
 const movies = require("./routes/movies");
+const tvShows = require("./routes/tv");
 
 // mount the videos functions to the /videos route
 app.use("/videos", videos);
 
-// mount the videos functions to the /videos route
+// mount the movies functions to the /movies route
 app.use("/movies", movies);
+
+// mount the tv functions to the /tv route
+app.use("/tv", tvShows);
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome to the API"); // Sets status to 201 (Created)
