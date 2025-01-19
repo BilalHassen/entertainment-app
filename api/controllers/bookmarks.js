e2ba16a0d1073de22eab3e2d2757898ed87ffa08
BilalHassen <bilalhassen097@gmail.com>
2025-01-19 15:53:30 -0500

    add a bookmark controller file and a query to fetch bookmarked Videos

---
+++ api/controllers/bookmarks.js
@@ -0,0 +1,33 @@
+const knex = require("knex")(require("../knexfile"));
+const {
+  collectDuplicateThumbnails,
+} = require("../helper/collectDuplicateThumbnails");
+
+async function getTvShows(req, res) {
+  try {
+    const bookmarkResponse = await knex("movies")
+      .join("thumbnails", "movies.id", "=", "thumbnails.movie_id")
+      .where("movies.is_recommended", "=", true)
+      .select(
+        "movies.id",
+        "movies.title",
+        "movies.year",
+        "movies.category",
+        "movies.rating",
+        "movies.is_bookmarked",
+        "movies.is_trending",
+        "movies.is_recommended",
+        "thumbnails.url"
+      );
+
+    const formattedBookmarkVideos =
+      collectDuplicateThumbnails(bookmarkResponse);
+    res.status(200).json(formattedBookmarkVideos);
+  } catch (err) {
+    res.status(500).json(`LoL an error occured ${err}`);
+  }
+}
+
+module.exports = {
+  getTvShows,
+};
